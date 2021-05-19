36c7595219d24d32947c16eae8e75545f808d573
Seif Sallam <Seif.ssm.97@outlook.com>
2021-05-19 11:08:23 +0200

    Add Objects

---
+++ Restaurents/Resturant.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurents
+{
+    public class Resturant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int  RegionId { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Address { get; set; }
+
+    }
+}
